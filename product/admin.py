681f5cff7302888f82530367312e2a4482d830e6
Brice LEROY <brice@pdb.fulham.com>
2009-10-19 20:10:33 +0000

    Remove uneeded imports and forms

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from models import Product, Import, Category
-from picture.admin import PictureInline
+from models import Product, Category
 from inventory.models import Inventory, Warehouse
 from inventory.admin import InventoryInline
-from django import forms
-from datetime import datetime
 from django.utils.translation import ugettext_lazy as _
 
 class ProductAdmin(admin.ModelAdmin):
@@ -16,13 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('ref', 'title',)
     list_display = ( 'ref', 'title',)
-
-class ImportAdminForm(forms.ModelForm):
-    search_fields = ('title',)
-    list_filter = ('is_imported', 'import_date', )
-    class Meta:
-        #model = Import
-        exclude = ('is_imported', 'import_date', 'updated_product', 'created_product')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
