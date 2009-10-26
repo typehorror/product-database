3f12e5562472cca41147a6e6e8fd11936998e666
Brice LEROY <brice@pdb.fulham.com>
2009-10-26 16:48:11 +0000

    Remove mass import functionality, import are too sensitive to be imported this way

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/inventory/import/(?P<object_id>[0-9]+)/import/','inventory.views.import_inventory'),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
